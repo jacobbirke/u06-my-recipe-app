22e741553187ca621d675d1e48f6fe55bd6083f1
jacobbirke <birkejacob@gmail.com>
2024-06-11 15:35:14 +0200

    update

--- recipe-app/src/app/auth.service.ts
+++ recipe-app/src/app/auth.service.ts
@@ -1,40 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = 'https://u06-fullstack-recipe-app-jacobbirke.onrender.com/api';
-  private tokenKey = 'authToken';  
 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { email, password }).pipe(
-      tap((response: any) => {
-        if (response && response.token) {
-          localStorage.setItem(this.tokenKey, response.token);
-        }
-      })
-    );
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
   }
 
   register(name: string, email: string, password: string, password_confirmation: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, { name, email, password, password_confirmation });
+    return this.http.post(`${this.apiUrl}/register`, { name, email, password, password_confirmation});
   }
 
   logout(): Observable<any> {
-    const token = localStorage.getItem(this.tokenKey);
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-
-    return this.http.post(`${this.apiUrl}/logout`, {}, { headers }).pipe(
-      tap(() => {
-        localStorage.removeItem(this.tokenKey);
-      })
-    );
+    return this.http.post(`${this.apiUrl}/logout`, {});
   }
 }
