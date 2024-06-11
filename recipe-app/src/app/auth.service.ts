86939ad7664668f4aaed735ca6bf06b1211b93ed
jacobbirke <birkejacob@gmail.com>
2024-06-11 14:44:32 +0200

    deployBackendURL

--- recipe-app/src/app/auth.service.ts
+++ recipe-app/src/app/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'https://u06-fullstack-recipe-app-jacobbirke.onrender.com/api';
 
   constructor(private http: HttpClient) { }
 
