71aa062daa7ac20a83a9629a7ded8724d6187e37
jacobbirke <birkejacob@gmail.com>
2024-06-11 16:02:31 +0200

    update

--- recipe-app/src/app/auth.service.ts
+++ recipe-app/src/app/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +12,24 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { email, password });
+    return this.http.post(`${this.apiUrl}/login`, { email, password }).pipe(
+      tap((response: any) => {
+        localStorage.setItem('token', response.token); // Save the token
+      })
+    );
   }
 
   register(name: string, email: string, password: string, password_confirmation: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, { name, email, password, password_confirmation});
+    return this.http.post(`${this.apiUrl}/register`, { name, email, password, password_confirmation });
   }
 
   logout(): Observable<any> {
     const token = localStorage.getItem('token'); // Assume token is stored in localStorage
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.post(`${this.apiUrl}/logout`, {}, { headers });
+    return this.http.post(`${this.apiUrl}/logout`, {}, { headers }).pipe(
+      tap(() => {
+        localStorage.removeItem('token'); // Remove the token on logout
+      })
+    );
   }
 }
